3eb2a6cf7954b3dde8b5778026279c68a6003d5f
Daniel <mixed.Pebble@gmail.com>
2018-08-21 16:41:56 -0400

    Added a button to the correct area and added a temporary image for it.
    Still need to: get the button in the right area. Make a custom image. Get windowed-fullscreen working

--- content_scripts/windowedFullScreen.js
+++ content_scripts/windowedFullScreen.js
@@ -6,18 +6,15 @@ This script adds an extra button to Youtube Videos.
 The extra button gives an extra view option: windowed fullscreen.
 */
 
-
-
-
 var viewArray = document.getElementsByClassName("ytp-right-controls");
-var defaultViewButton = viewArray[0];//This is the Theater/Default view button
-
-var btn = document.createElement("BUTTON");//Test button that is to be injected.
-var t = document.createTextNode("CLICK ME");//Text because it makes the button easier to see while testing
-btn.appendChild(t);                                
-defaultViewButton.appendChild(btn);                    
-
-
+var rightControls = viewArray[0];
+var btn = document.createElement("BUTTON");
+btn.setAttribute('class', 'WindowFullScreen');
+btn.setAttribute('viewBox', '0 0 36 36');
+//logo = browser.extension.getURL("resources/green.png");
+//btn.innerHTML='<img src=' +logo+'/>';
+btn.innerHTML='<img src="http://www.sandcmemories.com/t/Tsukineko/Brilliance_Dew_Drop_Spot_Pearlescent_Sky_Blue.gif" />';
+rightControls.appendChild(btn);                    
 
 console.log("FINISH"); //Useful for seeing if the script crashed early for whatever reason.
 
