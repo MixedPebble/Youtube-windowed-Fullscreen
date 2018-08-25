5b5fcf6e6503ffd81d0f1fb8b11c3e5027be6673
Daniel <mixed.Pebble@gmail.com>
2018-08-25 01:00:18 -0400

    New Button Update
    The function used to create the button that will be injected has been updated.
    The new function creates a copy of the existing Youtube theater button and alters its functionality.
    This method was chosen in case Youtube updates their video player HTML settings.

--- content_scripts/windowedFullScreen.js
+++ content_scripts/windowedFullScreen.js
@@ -1,20 +1,48 @@
+
+/*
+* This script adds an extra button to Youtube Videos.
+* The extra button gives an extra view option: windowed fullscreen.
+* References: https://developer.mozilla.org/en-US/docs/Web/API/Window/open
+*/
+
 console.log("START");
+var viewArray = document.getElementsByClassName("ytp-right-controls");
+var rightControls = viewArray[0];
+var btn = copyTheaterButton();
+//var btn = customWindowFullScreenButton();//Alternate way to get button
+btn.addEventListener("click",function(){
+    var strWindowFeatures = "menubar=no,location=no,resizable=no,scrollbars=no,status=no";
+    windowObjectReference = window.open("http://www.cnn.com/", "CNN_WindowName", strWindowFeatures);
+})
+rightControls.appendChild(btn);
+console.log("FINISH"); //Useful for seeing if the script crashed early for whatever reason.
+
 
 
 /*
-This script adds an extra button to Youtube Videos.
-The extra button gives an extra view option: windowed fullscreen.
+Creates a copy of existing Youtube button then alters it. 
+It is to be placed in Youtube video window.
+This is one of two ways that I am considering using for the button.
+The other way is: customWindowFullScreenButton
 */
-
-var viewArray = document.getElementsByClassName("ytp-right-controls");
-var rightControls = viewArray[0];
+function copyTheaterButton(){
+    var temp = document.getElementsByClassName("ytp-size-button ytp-button");
+    var temp2 = temp[0]; 
+    var temp3 = temp2.cloneNode(true);
+    return temp3;
+}
+/*
+Creates a new button to be placed in Youtube video window.
+This is one of two ways that I am considering using for creating the button.
+The other way is: copyTheaterButton()
+*/
+function customWindowFullScreenButton(){
 var btn = document.createElement("BUTTON");
 btn.setAttribute('class', 'WindowFullScreen');
 btn.setAttribute('viewBox', '0 0 36 36');
-//logo = browser.extension.getURL("resources/green.png");
-//btn.innerHTML='<img src=' +logo+'/>';
 btn.innerHTML='<img src="http://www.sandcmemories.com/t/Tsukineko/Brilliance_Dew_Drop_Spot_Pearlescent_Sky_Blue.gif" />';
-rightControls.appendChild(btn);                    
+return btn;
+}
 
-console.log("FINISH"); //Useful for seeing if the script crashed early for whatever reason.
+
 
